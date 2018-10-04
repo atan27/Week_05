1c82c48e77b48e9fbffcedd5a659256e76f8bc11
atan27 <atan27@illinois.edu>
2018-10-03 19:47:23 -0500

    followed lecture

--- src/App.js
+++ src/App.js
@@ -9,7 +9,32 @@ class App extends Component {
       "Add boba",
       "Drain water",
       "Add tea"
-    ]
+    ],
+    myTitle: "How to make boba",
+    newTodo: ""
+  };
+
+  handleChange = event => {
+    console.log(event.target.value)
+    this.setState({ newTodo: event.target.value });
+    console.log(this.state.newTodo)
+  };
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log("i submitted")
+    console.log(this.state.newTodo)
+    this.setState({
+      todos: [...this.state.todos, this.state.newTodo],
+      newTodo: ""
+    });
+  };
+  handleDelete = event => {
+    console.log("DELETE");
+    var newTodoList = [...this.state.todos]; 
+    var index = newTodoList.indexOf(event.target.value);
+    newTodoList.splice(index, 1);
+    this.setState({todos: newTodoList});
   };
 
   render() {
@@ -17,8 +42,28 @@ class App extends Component {
       <div>
         <h1> How to make boba at home</h1>
         {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
+          <li key={todo}> {todo}
+          {todo + " "}
+          <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}> + </button>
+          </form>
+          <button onClick={this.handleDelete()}>X</button>
+              <button  value={todo} onClick={this.handleDelete}>X</button>
+          </li>
         ))}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}> + </button>
+        </form>
       </div>
     );
   }
